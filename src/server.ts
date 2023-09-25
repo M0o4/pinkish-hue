6f38ce5868ce764cbb9e5f365b1b39621a91d103
Anuragkumarsah <asah2307@gmail.com>
2023-09-25 20:47:13 +0530

    added /popular endpoint and fixed episode number bug

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,54 @@
-const express = require('express');
-// import express from 'express';
-// import cors from 'cors';
-const cors = require('cors');
-import type { IRecentEpisodes } from './types/aniwatch.d.ts';
-import { fetchRecentEpisodes } from './utils/aniwatch_parser';
-
+const express = require("express");
+const cors = require("cors");
+import type { IRecentEpisodes } from "./types/aniwatch.d.ts";
+import { fetchRecentEpisodes, fetchPopular } from "./utils/aniwatch_parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: true,
-    credentials: true,
-    port: PORT,
+  origin: true,
+  credentials: true,
+  port: PORT,
 };
 
 app.use(cors(corsOptions));
 
-
-
-app.get('/', (req : any, res : any) => {
-    const homeinfo = {
-        message: 'Welcome to the Pinkish-Hue API! 🎉',
-        'endpoints (working)': [
-            "/recent-episodes"
-        ],
-        'endpoints (under development)': [
-            "/popular",
-            "/info/:id",
-            "/watch/:id",
-            "/:search-query"
-        ]
-    }
-    res.status(200).send(homeinfo);
+app.get("/", (req: any, res: any) => {
+  const homeinfo = {
+    message: "Welcome to the Pinkish-Hue API! 🎉",
+    "endpoints (working)": ["/recent-episodes", "/popular"],
+    "endpoints (under development)": [
+      "/info/:id",
+      "/watch/:id",
+      "/:search-query",
+    ],
+  };
+  res.status(200).send(homeinfo);
 });
 
-app.get('/recent-episodes', async (req : any, res : any) => {
-    try {
-        const page : number = parseInt(req.query?.page as string) || 1;
-        const data : IRecentEpisodes | string = await fetchRecentEpisodes({page: page});
-        return res.status(200).json(data);
-    } catch (error) {
-        res.status(400).send(`Some error occurred: ${error}`);
-    }
-})
+app.get("/recent-episodes", async (req: any, res: any) => {
+  try {
+    const page: number = parseInt(req.query?.page as string) || 1;
+    const data: IRecentEpisodes | string = await fetchRecentEpisodes({
+      page: page,
+    });
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(400).send(`Some error occurred: ${error}`);
+  }
+});
+
+app.get("/popular", async (req: any, res: any) => {
+  try {
+    const page: number = parseInt(req.query?.page as string) || 1;
+    const data: IRecentEpisodes | string = await fetchPopular({ page: page });
+    res.send(data);
+  } catch (error) {
+    res.status(400).send(`Some error occurred: ${error}`);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- src/utils/aniwatch_parser.ts
+++ src/utils/aniwatch_parser.ts
@@ -1,52 +1,121 @@
 // import axios from 'axios'
 // import {load} from 'cheerio'
 
-const axios = require('axios');
-const {load} = require('cheerio');
+const axios = require("axios");
+const { load } = require("cheerio");
 
-import type { IEpisodeData, IRecentEpisodes } from '../types/aniwatch.d.ts'
+import type { IEpisodeData, IRecentEpisodes } from "../types/aniwatch.d.ts";
 
-const BASE_URL : string = 'https://aniwatch.to'
+const BASE_URL: string = "https://aniwatch.to";
 
-const axiosInstance = axios.create({baseURL: BASE_URL})
+const axiosInstance = axios.create({ baseURL: BASE_URL });
 
-export const fetchRecentEpisodes = async ({ page = 1 }: {page : number}) : Promise<IRecentEpisodes | string> => {
+export const fetchRecentEpisodes = async ({
+  page = 1,
+}: {
+  page: number;
+}): Promise<IRecentEpisodes | string> => {
+  try {
+    const { data } = await axiosInstance.get(`/recently-updated?page=${page}`);
 
-    try {
-        const { data } = await axiosInstance.get(`/recently-updated?page=${page}`);
+    const $ = load(data);
 
-        const $ = load(data);
+    const hasNextPage =
+      $(".pagination > li").length > 0
+        ? $(".pagination > li").last().hasClass("active")
+          ? false
+          : true
+        : false;
 
-        const hasNextPage = $('.pagination > li').length > 0
-                            ? $('.pagination > li').last().hasClass('active')
-                              ? false 
-                              : true
-                            : false;
-        
-        const recentEpisodes: IEpisodeData[] = [];
+    const recentEpisodes: IEpisodeData[] = [];
 
-        $('div.film_list-wrap > div').each((ind :any, ele : any) => {
-            recentEpisodes.push({
-                id: $(ele).find('div.film-poster > a').attr('href')?.replace('/', '')!,
-                episodeNumber: parseInt($(ele).find('div.tick-eps').text().replace(/\s/g, '').replace('Ep', '').split('/')[0]),
-                title: $(ele).find('h3.film-name > a').attr('title')!,
-                alternateTitle: $(ele).find('h3.film-name > a').attr('data-jname')!,
-                image: $(ele).find('div.film-poster > img').attr('data-src')!,
-                description: $(ele).find('div.film-detail > div.description').text()!,
-                type: $(ele).find('div.film-detail > div.fd-infor > span.fdi-item').first().text(),
-                episodeDuration: $(ele).find('div.film-detail > div.fd-infor > span.fdi-duration').text()!,
-                totalEpisodes: null,
-            })
-        })
+    $("div.film_list-wrap > div").each((ind: any, ele: any) => {
+      recentEpisodes.push({
+        id: $(ele).find("div.film-poster > a").attr("href")?.replace("/", "")!,
+        episodeNumber: parseInt(
+          $(ele)
+            .find("div.tick-sub")
+            .text()
+            .replace(/\s/g, "")
+            .replace("Ep", "")
+            .split("/")[0]
+        ),
+        title: $(ele).find("h3.film-name > a").attr("title")!,
+        alternateTitle: $(ele).find("h3.film-name > a").attr("data-jname")!,
+        image: $(ele).find("div.film-poster > img").attr("data-src")!,
+        description: $(ele).find("div.film-detail > div.description").text()!,
+        type: $(ele)
+          .find("div.film-detail > div.fd-infor > span.fdi-item")
+          .first()
+          .text(),
+        episodeDuration: $(ele)
+          .find("div.film-detail > div.fd-infor > span.fdi-duration")
+          .text()!,
+        totalEpisodes: null,
+      });
+    });
 
-        return {
-            currentPage: page,
-            hasNextPage: hasNextPage,
-            results: recentEpisodes
-        };
-    } catch (error) {
-        return `Some error occurred: ${error}`;
-    }
+    return {
+      currentPage: page,
+      hasNextPage: hasNextPage,
+      results: recentEpisodes,
+    };
+  } catch (error) {
+    return `Some error occurred: ${error}`;
+  }
+};
 
-}
+export const fetchPopular = async ({
+  page = 1,
+}: {
+  page: number;
+}) : Promise<IRecentEpisodes | string> => {
+  try {
+    const { data } = await axiosInstance.get(`/top-airing?page=${page}`);
 
+    const $ = load(data);
+
+    const hasNextPage =
+      $(".pagination > li").length > 0
+        ? $(".pagination > li").last().hasClass("active")
+          ? false
+          : true
+        : false;
+    
+    const popular: IEpisodeData[] = [];
+
+    $("div.film_list-wrap > div").each((ind: any, ele: any) => {
+      popular.push({
+        id: $(ele).find("div.film-poster > a").attr("href")?.replace("/", "")!,
+        episodeNumber: parseInt(
+          $(ele)
+            .find("div.tick-sub")
+            .text()
+            .replace(/\s/g, "")
+            .replace("Ep", "")
+            .split("/")[0]
+        ),
+        title: $(ele).find("h3.film-name > a").attr("title")!,
+        alternateTitle: $(ele).find("h3.film-name > a").attr("data-jname")!,
+        image: $(ele).find("div.film-poster > img").attr("data-src")!,
+        description: $(ele).find("div.film-detail > div.description").text()!,
+        type: $(ele)
+          .find("div.film-detail > div.fd-infor > span.fdi-item")
+          .first()
+          .text(),
+        episodeDuration: $(ele)
+          .find("div.film-detail > div.fd-infor > span.fdi-duration")
+          .text()!,
+        totalEpisodes: null,
+      });
+    });
+
+    return {
+        currentPage: page,
+        hasNextPage: hasNextPage,
+        results: popular,
+    };
+  } catch (error) {
+    return `Some error occurred: ${error}`;
+  }
+};
